248d663110de07ffb3da396c41bfdbcd1553d293
Grant <gwensignak@gmail.com>
2019-09-08 16:30:06 -0600

    functionality auth works

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,3 +1,28 @@
+const getUser = async (req, res) => {
+    let db = req.app.get('db');
+    console.log('req.params: ', req.params);
+    console.log('');
+    const { user_id } = req.params;
+    // console.log('username: ', username)
+    // console.log('')
+    console.log('userId: ', user_id);
+    console.log('');
+    const userDisplay = await db.find_user_by_id([user_id]);
+    console.log("userDisplay: ", userDisplay);
+    console.log('');
+    res.status(200).send(userDisplay);
+};
+
+const getPosts = async (req, res) => {
+
+};
+
+const addPosts = (req, res) => {
+
+};
+
 module.export = {
-    
-}
+    getUser,
+    getPosts,
+    addPosts
+};
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default class Auth extends Component {
     state = {
@@ -6,22 +8,61 @@ export default class Auth extends Component {
         password: ''
     }
 
-    handleChange = () => {
-
+    handleChange = (event) => {
+        this.setState({
+            [event.target.key]: event.target.value
+        });
     }
 
     login = () => {
-
+        console.log(this.state);
+        const { username, password } = this.state;
+        console.log(username);
+        // currently only sending the catch err. 
+        axios.post('/api/auth/login', { username, password })
+            .then(res => {
+                this.props.setUser({ username, password });
+                this.props.history.push('/dashboard');
+            }).catch(err => {
+                alert('Username and/or Password does not exist. Try again');
+            })
+        console.log(username);
     }
-    
+
     register = () => {
-
+        console.log(this.state);
+        const { username, password, } = this.state;
+        console.log(username, password);
+        // only sending the catch. does not currently register new user. 
+        axios.post('/api/auth/register', { username, password })
+            .then(res => {
+                console.log(username);
+                this.props.setUser({ username, password });
+                console.log(username);
+                this.props.history.push('/dashboard');
+            }).catch(err => {
+                alert('Username and/or Password in use');
+            });
     }
-    
+
     render() {
         return (
             <div>
-                Authenication
+                <h1>Helo</h1>
+                <span>Username:
+                    <input type='text'
+                        key='username'
+                        onChange={this.handleChange} />
+                </span>
+                <span>Password:
+                    <input type='password'
+                        key='password'
+                        onChange={this.handleChange} />
+                </span>
+                <Link to='/dashboard'>
+                    <button onClick={this.login}>Login</button>
+                    <button onClick={this.register}>Register</button>
+                </Link>
             </div>
         )
     }
